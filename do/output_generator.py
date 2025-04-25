e0521ff0d76731d8d63b2f9b5ee2cc65d7c524a8
Phy1a <raphaelle.grimaldi@gmail.com>
2025-04-25 23:50:29 +0200

    Added untested code

--- do/output_generator.py
+++ do/output_generator.py
@@ -1,10 +1,10 @@
-import pandas as pd #pandas
-import os #os
+import pandas as pd
+import os 
 import re
 import requests
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
-import time
+from io import StringIO
 
 
 import warnings
@@ -48,77 +48,84 @@ if not(os.path.exists("output/organised_city_data.xlsx")):
 
     df_pop_histo = df_pop_histo.iloc[:,2:]#remove useless columns
     df_pop_histo["LIBGEO"] = df_pop_histo["LIBGEO"].apply(simplify_city) # uniform city names to duplicates
-
+    df_pop_histo = df_pop_histo.groupby(["DEP", "LIBGEO"], as_index=False)[df_pop_histo.columns[3:]].sum() # sum duplicates
 
     # Complete the DataFrame for the missing years
 
-    dep_dico = { # only Metropolitain France, code on the used website for GET requests
-        "01": "36", "02": "20", "03": "55", "04": "106", "05": "58", "06": "104","07": "62", "08": "74",
-        "09": "114","10": "19","11": "115","12": "4", "13": "125", "14": "22", "15": "120", "16": "40",
-        "17": "94", "18": "66", "19": "86", "21": "38", "22": "119", "23": "101", "24": "39", "25": "21",
-        "26": "123", "27": "72", "28": "54", "29": "138", "30": "99", "31": "78", "32": "59", "33": "60",
-        "34": "33", "35": "71", "36": "117", "37": "32", "38": "56", "39": "18", "40": "97", "41": "135",
-        "42": "47", "43": "96", "44": "16", "45": "65", "46": "17", "47": "89", "48": "127", "49": "132",
-        "50": "73", "51": "43", "52": "91", "53": "100", "54": "13", "55": "6", "56": "130", "57": "26",
-        "58": "69", "59": "11", "60": "14", "61": "128", "62": "42", "63": "111", "64": "2", "65": "76",
-        "66": "103", "67": "64", "68": "30", "69": "87", "70": "10", "71": "35", "72": "107", "73": "109",
-        "74": "51", "75": "129", "76": "8", "77": "68", "78": "24", "79": "80", "80": "27", "81": "98",
-        "82": "5", "83": "81", "84": "133", "85": "116", "86": "82", "87": "113", "88": "45", "89": "61",
-        "90": "136", "91": "25", "92": "137", "93": "141", "94": "46", "95": "44", "2A" : "84", "2B" : "92"
-    }
+    url = "http://cassini.ehess.fr/"
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            dep_dico = { # only Metropolitain France, code on the used website for GET requests
+                "01": "36", "02": "20", "03": "55", "04": "106", "05": "58", "06": "104","07": "62", "08": "74",
+                "09": "114","10": "19","11": "115","12": "4", "13": "125", "14": "22", "15": "120", "16": "40",
+                "17": "94", "18": "66", "19": "86", "21": "38", "22": "119", "23": "101", "24": "39", "25": "21",
+                "26": "123", "27": "72", "28": "54", "29": "138", "30": "99", "31": "78", "32": "59", "33": "60",
+                "34": "33", "35": "71", "36": "117", "37": "32", "38": "56", "39": "18", "40": "97", "41": "135",
+                "42": "47", "43": "96", "44": "16", "45": "65", "46": "17", "47": "89", "48": "127", "49": "132",
+                "50": "73", "51": "43", "52": "91", "53": "100", "54": "13", "55": "6", "56": "130", "57": "26",
+                "58": "69", "59": "11", "60": "14", "61": "128", "62": "42", "63": "111", "64": "2", "65": "76",
+                "66": "103", "67": "64", "68": "30", "69": "87", "70": "10", "71": "35", "72": "107", "73": "109",
+                "74": "51", "75": "129", "76": "8", "77": "68", "78": "24", "79": "80", "80": "27", "81": "98",
+                "82": "5", "83": "81", "84": "133", "85": "116", "86": "82", "87": "113", "88": "45", "89": "61",
+                "90": "136", "91": "25", "92": "137", "93": "141", "94": "46", "95": "44", "2A" : "84", "2B" : "92"
+            }
 
 
+            backup = df_pop_histo
 
-    backup = df_pop_histo
+            for year in range(1871,1800,-5):
+                df_year = pd.DataFrame()
+                for dep in list(dep_dico.keys()):
+                    url = f"http://cassini.ehess.fr/fr/PHP/exportPopCSV.php?csv=1&valider=validation&departement={dep_dico[dep]}&popBorneInf=0&popBorneSup=10000000&annee={year}"
+                    filename = f"output/cassini_dep_{dep}_{year}.csv"
 
-    for year in range(1871,1800,-5):
-        df_year = pd.DataFrame()
-        for dep in list(dep_dico.keys()):
-            url = f"http://cassini.ehess.fr/fr/PHP/exportPopCSV.php?csv=1&valider=validation&departement={dep_dico[dep]}&popBorneInf=0&popBorneSup=10000000&annee={year}"
-            filename = f"output/cassini_dep_{dep}_{year}.csv"
+                    try:
+                        # send GET request
+                        response = requests.get(url)
+                        
+                        # Check request success
+                        if response.status_code == 200:
+                            df_temp = pd.read_csv(StringIO(response.text), sep=";", encoding="utf-8", on_bad_lines='skip')
+                            
+                            if df_temp.shape[1] != 4:
+                                print(f"Badly formed data for dep {dep} in {year}, columns = {df_temp.columns}")
+                            else:
+                                df_temp.columns = ["INSEE", "LIBGEO", "PTOT"+str(year), "Year"]
+                                df_temp = df_temp[["LIBGEO", "PTOT"+str(year)]]
+                                df_temp["DEP"] = dep
+                                df_year = pd.concat([df_year, df_temp], ignore_index=True)
 
-            try:
-                # send GET request
-                response = requests.get(url)
+                        else:
+                            print(f"Error for {dep} in {year}: {response.status_code}")
+
+                    except requests.exceptions.RequestException as e:
+                        print(f"Connection error for {dep} in {year}: {e}")
+                    
                 
-            # Check request success
-                if response.status_code == 200:
-                    with open(filename, "w", encoding="utf-8") as f:
-                        f.write(response.text)
-                else:
-                    print(f"Error for {dep} in {year}: {response.status_code}")
-                
+                print(f"\n>>> Merging year {year}")
+                print(f"    df_year shape: {df_year.shape}")
+                print(f"    df_pop_histo shape after merge: {df_pop_histo.shape}")
 
-                df_temp = pd.read_csv(filename, sep=";", encoding="utf-8", on_bad_lines='skip') # read csv file
-                if df_temp.shape[1] != 4:
-                    print(f"Badly formed file : {filename}, columns = {df_temp.columns}")
-                    continue
+                df_year = df_year.drop_duplicates(subset=["LIBGEO", "DEP"], keep="first")
+                df_pop_histo = df_pop_histo.merge(df_year, on=["LIBGEO","DEP"], how="left")
 
-                df_temp.columns = ["INSEE", "LIBGEO", "PTOT"+str(year), "Year"]
-                df_temp = df_temp[["LIBGEO", "PTOT"+str(year)]] # Filter useful column
-                df_temp["DEP"] = dep
-                df_year = pd.concat([df_year, df_temp])
-                os.remove(filename)
+                duplicates = df_year.duplicated(subset=["LIBGEO", "DEP"]).sum()
+                if duplicates > 0:
+                    print(f"⚠️  {duplicates} duplicates in df_year for {year} on LIBGEO+DEP")
 
-            
-            except requests.exceptions.RequestException as e:
-                print(f"Connexion issues for {dep} in {year}: {e}")
-            
-        
-        print(f"\n>>> Merging year {year}")
-        print(f"    df_year shape: {df_year.shape}")
-        print(f"    df_pop_histo shape after merge: {df_pop_histo.shape}")
+                    
+            new_cols = [col for col in df_pop_histo if col not in backup.columns] # Identify new columns
+            df_pop_histo = df_pop_histo[[*backup.columns, *new_cols]] # Reorganize columns
+        else:
+            print(f"Le site a répondu avec le code : {response.status_code} ❌")
+    except requests.exceptions.RequestException as e:
+        print(f"Erreur de connexion : {e} ❌")
+    
+    print("Impossible to load datas before 1876 because the website isn't accessible")
+    
 
-        df_year = df_year.drop_duplicates(subset=["LIBGEO", "DEP"], keep="first") # remove duplicates
-        df_pop_histo = df_pop_histo.merge(df_year, on=["LIBGEO","DEP"], how="left")
-
-        duplicates = df_year.duplicated(subset=["LIBGEO", "DEP"]).sum()
-        if duplicates > 0:
-            print(f"⚠️  {duplicates} duplicates in df_year for {year} on LIBGEO+DEP")
-
-            
-    new_cols = [col for col in df_pop_histo if col not in backup.columns] # Identify new columns
-    df_pop_histo = df_pop_histo[[*backup.columns, *new_cols]] # Réorganize columns
+    
 
 
     df_total = df_uu_2024.merge(df_pop_histo, on=["LIBGEO","DEP"], how="inner") # fusion
