6e9f3b4c38f5b1e0d7f2dc9e5e5a03c3038c3fa2
Phy1a <raphaelle.grimaldi@gmail.com>
2025-04-25 19:38:49 +0200

    Last details

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd #pandas
-import os #os
-import geopandas as gpd
-import matplotlib.pyplot as plt
-import shapely
-
-
-lost_line = pd.DataFrame([{"DEP" : "01", "LIBGEO":"Ozan", "longitude" : "4.91667", "latitude" :"46.3833"}])
-print(lost_line.head())
